cadc664d815da27d4b81a06002d22aa0f6a88895
wisniax <101927988+wisniax@users.noreply.github.com>
2022-06-25 09:14:02 +0200

    Add project files.

---
+++ YAHAC/MVVM/ViewModel/BazaarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAHAC.MVVM.Model;
+using YAHAC.MVVM.View;
+
+namespace YAHAC.MVVM.ViewModel
+{
+	internal class BazaarViewModel
+	{
+		public BazaarView ViewModel { get; set; }
+
+		public BazaarViewModel()
+		{
+			ViewModel = new BazaarView();
+			ObservableCollection<object> items = new ObservableCollection<object>();
+			ViewModel.ItemsList.ItemsSource = items;
+			for (int i = 0; i < 400; i++)
+			{
+				items.Add(new MinecraftItemBox());
+			}
+		}
+
+	}
+}
---
+++ YAHAC/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAHAC.Core;
+using YAHAC.MVVM.View;
+
+namespace YAHAC.MVVM.ViewModel
+{
+	internal class MainViewModel : ObservableObject
+	{
+		public RelayCommand BazaarViewCommand { get; set; }
+		public RelayCommand AuctionHouseViewCommand { get; set; }
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set
+			{
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public MainViewModel()
+		{
+			BazaarViewCommand = new RelayCommand((o) => { CurrentView = new BazaarViewModel().ViewModel; });
+			AuctionHouseViewCommand = new RelayCommand((o) => { CurrentView = new AuctionHouseViewModel(); });
+			BazaarViewCommand.Execute(this);
+		}
+	}
+}
