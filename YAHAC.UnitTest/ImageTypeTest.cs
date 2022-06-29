db4708e38384a33de223d4a35d30db50f8f7c732
AlvaroBajceps <alvarobajceps@gmail.com>
2022-06-29 20:54:27 +0200

    updated to ITR 1.5.2; new unitTest for transparent top left pixel

--- YAHAC.UnitTest/ImageTypeTest.cs
+++ YAHAC.UnitTest/ImageTypeTest.cs
@@ -11,10 +11,27 @@ namespace YAHAC.UnitTest
 
 
         [TestCaseSource(nameof(ItemPreparator))]
-        public void ImageTypeTest_IsPngOrGif(AllItemsREPO.Item testItem)
+        public void ImageType_IsPngOrGifTest(AllItemsREPO.Item testItem)
         {
             var test = testItem.Texture.RawFormat.ToString();
             Assert.That(test, Is.EqualTo("Png") | Is.EqualTo("Gif"), "For item: " + testItem.id);
+        }
+
+        [TestCaseSource(nameof(ItemPreparator))]
+        public void ImageTopLeftPixel_IsTransparentTest(AllItemsREPO.Item testItem)
+        {
+            System.Drawing.Bitmap workingBitmap = (System.Drawing.Bitmap)testItem.Texture;
+
+            System.Drawing.Rectangle rect = new(0,0,workingBitmap.Width,workingBitmap.Height);
+
+            System.Drawing.Imaging.BitmapData bitmapData = workingBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            int renderCalcSize = Math.Abs(bitmapData.Stride) * bitmapData.Height;
+
+            byte[] bitmapPtr = new byte[renderCalcSize];
+
+            System.Runtime.InteropServices.Marshal.Copy(bitmapData.Scan0, bitmapPtr, 0, renderCalcSize);
+           
+            Assert.That(bitmapPtr[0], Is.EqualTo(0), "For item: " + testItem.id + " alpha is not 0!!!");
         }
 
         public static List<AllItemsREPO.Item> ItemPreparator()
